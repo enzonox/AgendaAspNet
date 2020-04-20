0e8086887b908ab32a51506863648c010663a25f
enzonox <enzod.leca@gmail.com>
2020-04-20 17:57:08 -0300

    Adicionar arquivos de projeto.

---
+++ Pages/Contatos/Create.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using AgendaAspNet.Models;
+
+namespace AgendaAspNet.Pages.Contatos
+{
+    public class CreateModel : PageModel
+    {
+        private readonly AgendaAspNet.Models.AppDbConnection _context;
+
+        public CreateModel(AgendaAspNet.Models.AppDbConnection context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Contato Contato { get; set; }
+
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Contatos.Add(Contato);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ Pages/Contatos/Index.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using AgendaAspNet.Models;
+
+namespace AgendaAspNet.Pages.Contatos
+{
+    public class IndexModel : PageModel
+    {
+        private readonly AgendaAspNet.Models.AppDbConnection _context;
+
+        public IndexModel(AgendaAspNet.Models.AppDbConnection context)
+        {
+            _context = context;
+        }
+
+        public IList<Contato> Contato { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Contato = await _context.Contatos.ToListAsync();
+        }
+    }
+}
